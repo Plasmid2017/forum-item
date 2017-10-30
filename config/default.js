ced4455546a18d126692e37f94799967eb8c7ede
Plasmid2017 <30335659+1183151232@users.noreply.github.com>
2017-10-30 17:17:31 +0800

    1

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: 3000,
+  session: {
+    secret: 'xinqingrizhi',
+    key: 'xinqingrizhi',
+    maxAge: 2592000000
+  },
+  mongodb: 'mongodb://localhost:27017/xinqingrizhi'
+};
